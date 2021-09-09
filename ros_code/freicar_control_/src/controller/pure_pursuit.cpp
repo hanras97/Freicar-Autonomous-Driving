d849242157b1ef1eb253bc49b534fd28d53ca5ae
Sougata Hanra <hanras97@gmail.com>
2021-09-09 03:51:59 +0200

    Update pure_pursuit.cpp

--- ros_code/freicar_control_/src/controller/pure_pursuit.cpp
+++ ros_code/freicar_control_/src/controller/pure_pursuit.cpp
@@ -31,12 +31,12 @@
 #include "std_msgs/Bool.h"
 #include "controller.h"
 #include <fstream>
+#include <sstream>
 #include <iostream>
 
 #include <kdl/frames.hpp>
 
 using std::string;
-
 
 
 
@@ -53,7 +53,10 @@ public:
 private:
     void controller_step(nav_msgs::Odometry odom);
     double ld_dist_;
-    int idx = 1 ;
+    int index = 1 ;
+    double sum_pos_error = 0;  //sg1
+    double mean_pos_error = 0;
+    const char *path="/home/freicar/freicar_ws/src/freicar-2020-exercises/04-01-control-exercise/freicar_control/position_error.txt";
 };
 
 
@@ -81,8 +84,6 @@ void PurePursuit::controller_step(nav_msgs::Odometry odom)
 {
 
     try{
-        std::shared_ptr<ros::NodeHandle> node_handle = std::make_shared<ros::NodeHandle>();
-        node_handle->getParam("carname", car_name);
         geometry_msgs::TransformStamped tf_msg;
         geometry_msgs::TransformStamped front_axis_tf_msg;
         tf2::Stamped<tf2::Transform> map_t_fa;
@@ -90,12 +91,15 @@ void PurePursuit::controller_step(nav_msgs::Odometry odom)
         front_axis_tf_msg = tf_buffer_.lookupTransform(map_frame_id_, front_axis_frame_id_, ros::Time(0));
         tf2::convert(tf_msg, map_t_fa);
 
-        if(path_.size() > 0 && idx < path_.size()-1)
+
+
+
+        if(path_.size() > 0 && index < path_.size()-1)
         {
 //            if(path_.size() > 0 && flag==true){
 //                for (i=0; i < path_.size()-1 ; i++)
 //                {
-//                    if (distance(path_.at(idx).getOrigin().x(), path_.at(idx).getOrigin().y(),path_.at(idx).getOrigin().z(), map_t_fa.getOrigin().x(), map_t_fa.getOrigin().y(), map_t_fa.getOrigin().z()) > ld_dist_)
+//                    if (distance(path_.at(index).getOrigin().x(), path_.at(index).getOrigin().y(),path_.at(index).getOrigin().z(), map_t_fa.getOrigin().x(), map_t_fa.getOrigin().y(), map_t_fa.getOrigin().z()) > ld_dist_)
 //                    {
 //                        if(distance(path_.at(i).getOrigin().x(), path_.at(i).getOrigin().y(),path_.at(i).getOrigin().z(), map_t_fa.getOrigin().x(), map_t_fa.getOrigin().y(), map_t_fa.getOrigin().z()) < distance(path_.at(mini).getOrigin().x(), path_.at(mini).getOrigin().y(),path_.at(mini).getOrigin().z(), map_t_fa.getOrigin().x(), map_t_fa.getOrigin().y(), map_t_fa.getOrigin().z())) {
 //                            mini = i;
@@ -104,71 +108,85 @@ void PurePursuit::controller_step(nav_msgs::Odometry odom)
 //                }
 //                flag = false;
 //            }
-//            if(idx >= mini)
+//            if(index >= mini)
 //            {
-                sendGoalMsg(false);
+            sendGoalMsg(false);
 //            completion_advertised_ = false;
+            // sendGoalMsg(false);
+            float x = map_t_fa.getRotation().x();
+            float y = map_t_fa.getRotation().y();
+            float z = map_t_fa.getRotation().z();
+            float w = map_t_fa.getRotation().w();
 
-                float x = map_t_fa.getRotation().x();
-                float y = map_t_fa.getRotation().y();
-                float z = map_t_fa.getRotation().z();
-                float w = map_t_fa.getRotation().w();
-
-                tf2::Quaternion q(x,y,z,w);
-                tf2::Matrix3x3 m(q);
-                double roll, pitch, yaw;
-                m.getRPY(roll, pitch, yaw);
+            tf2::Quaternion q(x,y,z,w);
+            tf2::Matrix3x3 m(q);
+            double roll, pitch, yaw;
+            m.getRPY(roll, pitch, yaw);
 
 
 
-                float alpha = atan2(path_[idx].getOrigin().y() - map_t_fa.getOrigin().y(), path_[idx].getOrigin().x() - map_t_fa.getOrigin().x()) - yaw;
+            float alpha = atan2(path_[index].getOrigin().y() - map_t_fa.getOrigin().y(), path_[index].getOrigin().x() - map_t_fa.getOrigin().x()) - yaw;
 
-                double steering_angle = atan2(2 * L_ * sin(alpha), ld_dist_);
-                float pid_vel_out = 0.0;
-                if (des_v_ >= 0) {
-                    pid_vel_out = vel_pid.step((des_v_ - odom.twist.twist.linear.x), ros::Time::now());
-                } else {
-                    pid_vel_out = des_v_;
+            double steering_angle = atan2(2 * L_ * sin(alpha), ld_dist_);
+            float pid_vel_out = 0.0;
+            if (des_v_ >= 0) {
+                pid_vel_out = vel_pid.step((des_v_ - odom.twist.twist.linear.x), ros::Time::now());
+                //std::cout << "throttle" << pid_vel_out << std::endl;
+            } else {
+                pid_vel_out = des_v_;
 
-                    vel_pid.resetIntegral();
-                }
+                vel_pid.resetIntegral();
+            }
 
-                cmd_control_.steering =steering_angle / (70.0 * M_PI / 180.0); //  DUMMY_STEERING_ANGLE should be a value in degree
-                cmd_control_.throttle = pid_vel_out;
-                cmd_control_.throttle_mode = 0;
-                cmd_control_.throttle = std::min(cmd_control_.throttle, 0.08f);
-                cmd_control_.throttle = std::max(std::min((double) cmd_control_.throttle, 1.0), 0.0);
+            cmd_control_.steering =steering_angle / (70.0 * M_PI / 180.0); //  DUMMY_STEERING_ANGLE should be a value in degree
+            cmd_control_.throttle = pid_vel_out;
+            cmd_control_.throttle_mode = 0;
+            cmd_control_.throttle = std::min(cmd_control_.throttle, 0.08f);
+            cmd_control_.throttle = std::max(std::min((double) cmd_control_.throttle, 1.0), 0.0);
+//            std::cout << "throttle" << cmd_control_.throttle << std::endl;
+            pub_acker_.publish(cmd_control_);
+
+            if(stop_sign == true){
+
+                cmd_control_.throttle = 0;
                 pub_acker_.publish(cmd_control_);
-
-                if(stop_sign == true){
-
-                    cmd_control_.throttle = 0;
+                std::cout<< "STOP IN CONTROLLER" << cmd_control_.throttle << std::endl;
+                ros::Duration(1).sleep();
+                cmd_control_.throttle = 0.07;
+                cmd_control_.steering =steering_angle / (70 * M_PI / 180.0);
+                std::cout<< "START AGAIN " << cmd_control_.throttle << std::endl;
+                pub_acker_.publish(cmd_control_);
+            }
+            /*/
+                if( min_depth_dist < 0.19 && min_depth_dist > 0.01 ){
+                    cmd_control_.throttle = -0.09;
+                    cmd_control_.steering = 0;
                     pub_acker_.publish(cmd_control_);
-                    std::cout<< "Stop sign detected! Stop Controller" << cmd_control_.throttle << std::endl;
-                    ros::Duration(1).sleep();
-                    cmd_control_.throttle = 0.07;
-                    cmd_control_.steering =steering_angle / (70 * M_PI / 180.0);
-                    std::cout<< "Starting Again " << cmd_control_.throttle << std::endl;
+                    ros::Duration(0.40).sleep();
+                    cmd_control_.throttle = 0.06;
+                    cmd_control_.steering =steering_angle / (70.0 * M_PI / 180.0) + 0.05;
+                    std::cout<< "START AGAIN " << cmd_control_.throttle << std::endl;
                     pub_acker_.publish(cmd_control_);
                 }
-//
-//                if(overtake == true){
-//                    std::cout<< "starting overtake" << std::endl;
-//                    cmd_control_.throttle = -0.1;
-//                    cmd_control_.steering = 0;
-//                    pub_acker_.publish(cmd_control_);
-//                    ros::Duration(0.50).sleep();
-//                    cmd_control_.throttle = 0.06;
-//                    cmd_control_.steering =steering_angle / (70.0 * M_PI / 180.0);
-//                    pub_acker_.publish(cmd_control_);
-//                    idx = 0;
-//
-//                }
-//
-                if (HLC_msg.name == car_name){
-                    idx = 0;
-//                }
+                */
 
+            if(overtake == true){
+                std::cout<< "starting overtake" << std::endl;
+                cmd_control_.throttle = -0.1;
+                cmd_control_.steering = 0;
+                pub_acker_.publish(cmd_control_);
+                ros::Duration(0.50).sleep();
+                cmd_control_.throttle = 0.06;
+                cmd_control_.steering =steering_angle / (70.0 * M_PI / 180.0);
+                pub_acker_.publish(cmd_control_);
+                index = 0;
+
+            }
+            //HERE
+            if (HLC_msg.name == "freicar_1"){
+                index = 0;
+            }
+            //HERE
 //             else {
 //                 cmd_control_.steering =steering_angle / (70.0 * M_PI / 180.0); //  DUMMY_STEERING_ANGLE should be a value in degree
 //                 cmd_control_.throttle = 0.1;
@@ -176,48 +194,56 @@ void PurePursuit::controller_step(nav_msgs::Odometry odom)
 //                 pub_acker_.publish(cmd_control_);
 //             }
 //            pub_acker_.publish(cmd_control_);
-//                float pos_error_x = path_[idx].getOrigin().x() - map_t_fa.getOrigin().x();
-//                float pos_error_y = path_[idx].getOrigin().y() - map_t_fa.getOrigin().y();
-//
-//                float pos_error = 0;
-//                //sg1 Calculate pos_error and moving mean over time as idx increase (i.e mean position error over time)
-//                //pos_error = distance(path_[idx].getOrigin().x(),path_[idx].getOrigin().y(),path_[idx].getOrigin().z(),
-//                //                   map_t_fa.getOrigin().x(),map_t_fa.getOrigin().y(), map_t_fa.getOrigin().z());
-//                pos_error = fabs(sin(alpha) * ld_dist_);
-//                if (fabs(pos_error_x) < 0.8 && fabs(pos_error_y) < 0.8)
-//                {
+            float pos_error_x = path_[index].getOrigin().x() - map_t_fa.getOrigin().x();
+            float pos_error_y = path_[index].getOrigin().y() - map_t_fa.getOrigin().y();
 
-//                    sum_pos_error = sum_pos_error + pos_error;
-//                    mean_pos_error = sum_pos_error / idx;
-////                std::cout << "pos_error ist " << pos_error << "at time " << idx << std::endl;
-//                    std::fstream file;
-//                    file.open(path, std::fstream::app);
-//                    if (!file.is_open()) {
-//                        std::cout << "File doesnt exist ";
-//                    }
-//                    file << pos_error << ",";
-//                    file.close();
-                    idx = idx + 1;
-                    std::cout << "idx "<< idx<< std::endl;
-                    if (idx >= path_.size()-4)
-                    {
-                        std::cout << "Goal Reached " << idx << std::endl;
-                        sendGoalMsg(true);
-//                    idx = 0;
-                        ros::Duration(1).sleep();
-                        sendGoalMsg(false);
-                        idx = 1;
-                        //flag = true;
+            float pos_error = 0;
+            //sg1 Calculate pos_error and moving mean over time as idx increase (i.e mean position error over time)
+            //pos_error = distance(path_[index].getOrigin().x(),path_[index].getOrigin().y(),path_[index].getOrigin().z(),
+            //                   map_t_fa.getOrigin().x(),map_t_fa.getOrigin().y(), map_t_fa.getOrigin().z());
+            pos_error = fabs(sin(alpha) * ld_dist_);
+            if (fabs(pos_error_x) < 0.8 && fabs(pos_error_y) < 0.8)
+            {
 
-                    }
+                sum_pos_error = sum_pos_error + pos_error;
+                mean_pos_error = sum_pos_error / index;
+//                std::cout << "pos_error ist " << pos_error << "at time " << index << std::endl;
+                std::fstream file;
+                file.open(path, std::fstream::app);
+                if (!file.is_open()) {
+                    std::cout << "File doesnt exist ";
                 }
+                file << pos_error << ",";
+                file.close();
+                index = index + 1;
+                std::cout << "index "<< index<< std::endl;
+                if (index >= path_.size()-4)
+                {
+                    std::cout << "GOAL REACHED BY PURE PURSUIT " << index << std::endl;
+                    sendGoalMsg(true);
+//                    index = 0;
+                    ros::Duration(1).sleep();
+                    sendGoalMsg(false);
+                    index = 1;
+                    //flag = true;
+
+                }
+
+
+
             }
+            //std::cout << "index" << index << std::endl;
+            //std::cout << "mean_pos_error ist " << mean_pos_error << "at time " << index << std::endl;
+        }
 //
 //
 //        }
 //
         else{
+//            cmd_control_.throttle = 0;
             pub_acker_.publish(cmd_control_);
+
+
         }
 
 
