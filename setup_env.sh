a6de5b2f91559b61c35ff84d7a72ec60ea6da14c
vertensj <vertensj@informatik.uni-freiburg.de>
2021-09-09 11:29:38 +0200

    install torch with correct cuda version

--- setup_env.sh
+++ setup_env.sh
@@ -3,12 +3,11 @@ echo "Installing dependencies..."
 # install anything...
 yes | pip3 install mean-average-precision
 yes | pip3 install numpy
-yes | pip3 install bounding_box 
-yes | pip3 install torch
+yes | pip3 install bounding_box
+pip3 install --no-input torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 yes | pip3 install pyyaml
 yes | pip3 install webcolors
 yes | pip3 install matplotlib
-yes | pip3 install torchvision
 yes | pip3 install webcolors
 yes | pip3 install visdom
 yes | pip3 install tqdm
