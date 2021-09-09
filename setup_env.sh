1f70a52196ef90e076cf92545678a5944136a115
Sougata Hanra <hanras97@gmail.com>
2021-09-09 07:22:53 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,16 @@
 echo "Installing dependencies..."
 
 # install anything...
+yes | pip3 install mean-average-precision
+yes | pip3 install numpy
+yes | pip3 install bounding_box 
+yes | pip3 install torch
+yes | pip3 install pyyaml
+yes | pip3 install webcolors
+yes | pip3 install matplotlib
+yes | pip3 install torchvision
+yes | pip3 install webcolors
+yes | pip3 install visdom
+yes | pip3 install tqdm
+yes | pip3 install opencv-python
+# install anything...
