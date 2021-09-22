6f0c40dc8803e4f4c03b1c4bfafc661902651be7
Sougata Hanra <hanras97@gmail.com>
2021-09-22 15:24:46 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -4,7 +4,8 @@ echo "Installing dependencies..."
 yes | pip3 install mean-average-precision
 yes | pip3 install numpy
 yes | pip3 install bounding_box
-pip3 install --no-input torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+yes | pip3 install torch
+#pip3 install --no-input torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 yes | pip3 install pyyaml
 yes | pip3 install webcolors
 yes | pip3 install matplotlib
